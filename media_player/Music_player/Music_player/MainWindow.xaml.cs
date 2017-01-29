5a0d9b5f3e68265ca0ccd4d895b456387b770f0b
mariavila <mariava.1213@gmail.com>
2017-01-29 08:50:41 +0100

    reproductor

---
+++ media_player/Music_player/Music_player/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WMPLib;
+using System.Windows.Controls.Primitives;
+
+
+namespace Music_player
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //private double selectedMarker = 0;
+        WindowsMediaPlayer player;
+        public MainWindow()
+        {
+            InitializeComponent();
+            player = new WindowsMediaPlayer();
+            player.URL = @"C:\Users\Maria\Desktop\pc antic\Musica\Vídeoclip  M'agrada el Ping pong.mp3";
+            player.controls.stop();
+        }
+
+        void Play(object sender, RoutedEventArgs args)
+        {
+            ToggleButton tb = (ToggleButton)sender;
+            Console.WriteLine((bool)tb.IsChecked);
+            if ((bool)tb.IsChecked)
+            {
+                player.controls.play();
+            }
+            else {
+                player.controls.pause();
+            }
+        }
+        void Next(object sender, RoutedEventArgs args)
+        {
+            player.controls.stop();
+            //player.URL = getPATH;
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            //player.controls.currentPosition =Convert.ToDouble(e);
+        }
+    }
+}
